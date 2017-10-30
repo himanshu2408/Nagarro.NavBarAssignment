a8e5616b0381dd205f526743b00dc274cd225430
Himanshu Singh <himanshu.singh@nagarro.com>
2017-10-30 22:08:58 +0530

    navBar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello/hello.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HelloComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule
